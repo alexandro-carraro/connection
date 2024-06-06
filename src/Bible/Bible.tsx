0fa5bfcce824b063062d610874c703b0de7ece20
Alexandro <alexandro_cristian@hotmail.com>
2024-06-05 21:09:05 -0300

    atualizaçao

--- src/Bible/Bible.tsx
+++ src/Bible/Bible.tsx
@@ -1,24 +1,22 @@
 import { useState } from 'react';
 
-
 const BibleVerse = () => {
-const [verse, setVerse] = useState('');
+  const [verse, setVerse] = useState('');
 
+  const generateVerse = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
 
-const generateVerse = () => {
+    fetch(`https://www.abibliadigital.com.br/api/verses/nvi/random`)
+      .then(response => response.json()) 
+      .then(data => setVerse(`${data.book.name} ${data.chapter}:${data.number}: ${data.text}`));
+  };
 
-  fetch(`https://www.abibliadigital.com.br/api/verses/nvi/random`)
-    .then(response => response.json()) 
-    .then(data => setVerse(`${data.book.name} ${data.chapter}:${data.number}: ${data.text}`));
-};
-
-return (
+  return (
     <div>
-      <span className='generate-verse' onClick={generateVerse}>Clique aqui e gere um versículo da Bíblia</span>
+      <button className='generate-verse' onClick={generateVerse}>Clique aqui e gere um versículo da Bíblia</button>
       <p className='bible-verse'>{verse}</p>
     </div>
   );
-  };
-
+};
 
 export default BibleVerse;
